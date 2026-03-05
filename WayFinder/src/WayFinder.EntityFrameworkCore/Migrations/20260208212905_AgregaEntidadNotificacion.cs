b46b9db9cfbac9daedd970cc793e861a4a74ffe9
Valentina <valenschenberger0304@gmail.com>
2026-03-05 17:51:05 -0300

    correcion de errores en backend

--- WayFinder/src/WayFinder.EntityFrameworkCore/Migrations/20260208212905_AgregaEntidadNotificacion.cs
+++ WayFinder/src/WayFinder.EntityFrameworkCore/Migrations/20260208212905_AgregaEntidadNotificacion.cs
@@ -11,25 +11,6 @@ namespace WayFinder.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<Guid>(
-                name: "DeleterId",
-                table: "AppCalificaciones",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "DeletionTime",
-                table: "AppCalificaciones",
-                type: "datetime2",
-                nullable: true);
-
-            migrationBuilder.AddColumn<bool>(
-                name: "IsDeleted",
-                table: "AppCalificaciones",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
-
             migrationBuilder.CreateTable(
                 name: "ListasSeguimiento",
                 columns: table => new
@@ -84,18 +65,6 @@ namespace WayFinder.Migrations
 
             migrationBuilder.DropTable(
                 name: "Notificaciones");
-
-            migrationBuilder.DropColumn(
-                name: "DeleterId",
-                table: "AppCalificaciones");
-
-            migrationBuilder.DropColumn(
-                name: "DeletionTime",
-                table: "AppCalificaciones");
-
-            migrationBuilder.DropColumn(
-                name: "IsDeleted",
-                table: "AppCalificaciones");
         }
     }
 }
